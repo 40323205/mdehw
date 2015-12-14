89760435b6372984a22c6e9e58a3de757b809534
40323205 <chendu85525@gmail.com>
2015-12-14 10:18:04 +0800

    1214 updates_4

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -32,8 +32,8 @@ LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
                 ('全班協同網站', 'http://2015fallhw.github.io/2015fallcadpb/'))
 
 # Social widget
-SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('github', 'http://github.com/ametaireau'),)
+SOCIAL = (('My Github', 'https://github.com/40323205/mdehw/tree/gh-pages'),
+          ('2015fallhw/2015fallcadpb', 'https://github.com/2015fallhw/2015fallcadpb/tree/gh-pages'),)
 
 DEFAULT_PAGINATION = 10
 
